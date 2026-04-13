cfbdc43a9017e44797907f26daf6a39f951decca
Loc Bui <locbuigia7101996@gmail.com>
2026-04-12 21:14:52 -0700

    Added Carousel for Home screen, added About screen and WIP for Products screen

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import HeroCarousel from "./HeroCarousel";
+import { Button } from "../ui/button";
+import Link from "next/link";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="grid grid-cols-1 lg:grid-cols-2 gap-24 items-center">
+      <div>
+        <h1 className="max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl">
+          We are changing the way people shop
+        </h1>
+        <p className="mt-8 max-w-xl text-lg leading-8 text-muted-foreground">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+          tempor lobortis leo, id rhoncus lorem condimentum sit amet. Fusce eu
+          sem luctus, tincidunt orci sed, rhoncus ligula. Donec scelerisque
+          finibus arcu quis varius. Nulla vitae lorem ornare metus mollis
+          consequat.
+        </p>
+        <Button asChild size="lg" className="mt-10">
+          <Link href={"/products"}>Our Products</Link>
+        </Button>
+      </div>
+      <HeroCarousel />
+    </section>
+  );
 };
 
 export default Hero;
